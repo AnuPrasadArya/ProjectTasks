a8653c4c774e5c373d10c1b691879a7f5c35233a
AnuPrasad <tradeanuprasad@gmail.com>
2025-07-11 18:39:45 +0530

    XUnit Testing

---
+++ ProjectTest/ProjectTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectTask.API.Controllers;
+using ProjectTask.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTest
+{
+    public class ProjectTest
+    {
+        [Fact]
+       
+        public async Task CreateProject_Should_Add_Project_To_Db()
+        {
+            // Arrange
+            var context = DbContextHelper.GetInMemoryDbContext();
+
+            var controller = new ProjectController(context);
+
+            var newProject = new Projects
+            {
+                Name = "Test Project",
+                Description = "Test Description",
+                UserId = 1
+            };
+
+            // Act
+            var result = await controller.CreateProject(newProject);
+
+            // Assert
+            var createdResult = Assert.IsType<OkObjectResult>(result);
+            var addedProject = await context.Projects.FirstOrDefaultAsync(p => p.Name == "Test Project");
+            Assert.NotNull(addedProject);
+            Assert.Equal("Test Description", addedProject.Description);
+        }
+    }
+}
---
+++ ProjectTest/TaskTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectTask.API.Controllers;
+using ProjectTask.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTest
+{
+    public class TaskTest
+    {
+        [Fact]
+        public async Task CreateTask_Should_Add_Task_To_Db()
+        {
+            
+            var context = DbContextHelper.GetInMemoryDbContext();
+
+            // Add a project first because task needs ProjectId
+            var project = new Projects
+            {
+                Name = "Project for Task",
+                Description = "Desc",
+                UserId = 1
+            };
+            context.Projects.Add(project);
+            await context.SaveChangesAsync();
+
+            var controller = new TaskController(context);
+
+            var newTask = new ProjectTasks
+            {
+                Title = "wms",
+                Description = "warehouse management",
+                DueDate = DateTime.Now.AddDays(5),
+                IsCompleted = false,
+                ProjectId = project.Id,
+                 
+            };
+
+            // Act
+            var result = await controller.CreateTask(newTask);
+
+            // Assert
+            var createdResult = Assert.IsType<OkObjectResult>(result);
+            var addedTask = await context.Tasks.FirstOrDefaultAsync(t => t.Title == "New Task");
+            Assert.NotNull(addedTask);
+            Assert.Equal("Task Desc", addedTask.Description);
+        }
+    }
+}
